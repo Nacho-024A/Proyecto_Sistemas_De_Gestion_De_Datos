96b0be63539761be8a707ebdda87949bf3738523
Juan Fernando Hernández Arenas <141139460+RicketyDream@users.noreply.github.com>
2025-11-04 13:21:04 -0500

    Add files via upload

---
+++ BaseDeDatos/BaseDeDatos/EAFIT-BaseDeDatos.UI/Horarios.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace EAFIT_BaseDeDatos.UI
+{
+    partial class Horarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LblID = new Label();
+            TxtIDHorario = new TextBox();
+            LblCurso = new Label();
+            TxtCursoID = new TextBox();
+            LblSalon = new Label();
+            TxtSalonID = new TextBox();
+            LblDia = new Label();
+            TxtDiaSemana = new TextBox();
+            LblInicio = new Label();
+            LblFin = new Label();
+            DtpHoraInicio = new DateTimePicker();
+            DtpHoraFin = new DateTimePicker();
+            BtnIngresar = new Button();
+            BtnUpdate = new Button();
+            BtnDelete = new Button();
+            BtnConsultar = new Button();
+            BtnSalir = new Button();
+            BtnNuevo = new Button();
+            LblTitle = new Label();
+            SuspendLayout();
+            // 
+            // LblID
+            // 
+            LblID.AutoSize = true;
+            LblID.Location = new Point(61, 76);
+            LblID.Name = "LblID";
+            LblID.Size = new Size(64, 15);
+            LblID.TabIndex = 0;
+            LblID.Text = "ID Horario:";
+            // 
+            // TxtIDHorario
+            // 
+            TxtIDHorario.Location = new Point(271, 76);
+            TxtIDHorario.Name = "TxtIDHorario";
+            TxtIDHorario.Size = new Size(150, 23);
+            TxtIDHorario.TabIndex = 1;
+            // 
+            // LblCurso
+            // 
+            LblCurso.AutoSize = true;
+            LblCurso.Location = new Point(61, 116);
+            LblCurso.Name = "LblCurso";
+            LblCurso.Size = new Size(55, 15);
+            LblCurso.TabIndex = 2;
+            LblCurso.Text = "ID Curso:";
+            // 
+            // TxtCursoID
+            // 
+            TxtCursoID.Location = new Point(271, 116);
+            TxtCursoID.Name = "TxtCursoID";
+            TxtCursoID.Size = new Size(150, 23);
+            TxtCursoID.TabIndex = 3;
+            // 
+            // LblSalon
+            // 
+            LblSalon.AutoSize = true;
+            LblSalon.Location = new Point(61, 156);
+            LblSalon.Name = "LblSalon";
+            LblSalon.Size = new Size(53, 15);
+            LblSalon.TabIndex = 4;
+            LblSalon.Text = "ID Salón:";
+            // 
+            // TxtSalonID
+            // 
+            TxtSalonID.Location = new Point(271, 156);
+            TxtSalonID.Name = "TxtSalonID";
+            TxtSalonID.Size = new Size(150, 23);
+            TxtSalonID.TabIndex = 5;
+            // 
+            // LblDia
+            // 
+            LblDia.AutoSize = true;
+            LblDia.Location = new Point(61, 196);
+            LblDia.Name = "LblDia";
+            LblDia.Size = new Size(72, 15);
+            LblDia.TabIndex = 6;
+            LblDia.Text = "Día Semana:";
+            // 
+            // TxtDiaSemana
+            // 
+            TxtDiaSemana.Location = new Point(271, 196);
+            TxtDiaSemana.Name = "TxtDiaSemana";
+            TxtDiaSemana.Size = new Size(150, 23);
+            TxtDiaSemana.TabIndex = 7;
+            // 
+            // LblInicio
+            // 
+            LblInicio.AutoSize = true;
+            LblInicio.Location = new Point(61, 236);
+            LblInicio.Name = "LblInicio";
+            LblInicio.Size = new Size(68, 15);
+            LblInicio.TabIndex = 8;
+            LblInicio.Text = "Hora Inicio:";
+            // 
+            // LblFin
+            // 
+            LblFin.AutoSize = true;
+            LblFin.Location = new Point(61, 276);
+            LblFin.Name = "LblFin";
+            LblFin.Size = new Size(55, 15);
+            LblFin.TabIndex = 10;
+            LblFin.Text = "Hora Fin:";
+            // 
+            // DtpHoraInicio
+            // 
+            DtpHoraInicio.Format = DateTimePickerFormat.Time;
+            DtpHoraInicio.Location = new Point(271, 236);
+            DtpHoraInicio.Name = "DtpHoraInicio";
+            DtpHoraInicio.Size = new Size(150, 23);
+            DtpHoraInicio.TabIndex = 11;
+            DtpHoraInicio.Value = new DateTime(2025, 11, 4, 0, 0, 0, 0);
+            // 
+            // DtpHoraFin
+            // 
+            DtpHoraFin.Format = DateTimePickerFormat.Time;
+            DtpHoraFin.Location = new Point(271, 276);
+            DtpHoraFin.Name = "DtpHoraFin";
+            DtpHoraFin.Size = new Size(150, 23);
+            DtpHoraFin.TabIndex = 12;
+            DtpHoraFin.Value = new DateTime(2025, 11, 4, 12, 0, 0, 0);
+            // 
+            // BtnIngresar
+            // 
+            BtnIngresar.BackColor = SystemColors.MenuHighlight;
+            BtnIngresar.Font = new Font("Segoe UI", 13F);
+            BtnIngresar.Location = new Point(93, 349);
+            BtnIngresar.Name = "BtnIngresar";
+            BtnIngresar.Size = new Size(80, 40);
+            BtnIngresar.TabIndex = 13;
+            BtnIngresar.Text = "Insertar";
+            BtnIngresar.UseVisualStyleBackColor = false;
+            BtnIngresar.Click += BtnIngresar_Click;
+            // 
+            // BtnUpdate
+            // 
+            BtnUpdate.BackColor = SystemColors.MenuHighlight;
+            BtnUpdate.Font = new Font("Segoe UI", 13F);
+            BtnUpdate.Location = new Point(203, 349);
+            BtnUpdate.Name = "BtnUpdate";
+            BtnUpdate.Size = new Size(100, 40);
+            BtnUpdate.TabIndex = 14;
+            BtnUpdate.Text = "Actualizar";
+            BtnUpdate.UseVisualStyleBackColor = false;
+            BtnUpdate.Click += BtnUpdate_Click;
+            // 
+            // BtnDelete
+            // 
+            BtnDelete.BackColor = SystemColors.MenuHighlight;
+            BtnDelete.Font = new Font("Segoe UI", 13F);
+            BtnDelete.Location = new Point(333, 349);
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.Size = new Size(100, 40);
+            BtnDelete.TabIndex = 15;
+            BtnDelete.Text = "Eliminar";
+            BtnDelete.UseVisualStyleBackColor = false;
+            BtnDelete.Click += BtnDelete_Click;
+            // 
+            // BtnConsultar
+            // 
+            BtnConsultar.BackColor = SystemColors.MenuHighlight;
+            BtnConsultar.Font = new Font("Segoe UI", 13F);
+            BtnConsultar.Location = new Point(463, 349);
+            BtnConsultar.Name = "BtnConsultar";
+            BtnConsultar.Size = new Size(100, 40);
+            BtnConsultar.TabIndex = 16;
+            BtnConsultar.Text = "Consultar";
+            BtnConsultar.UseVisualStyleBackColor = false;
+            BtnConsultar.Click += BtnConsultar_Click;
+            // 
+            // BtnSalir
+            // 
+            BtnSalir.BackColor = SystemColors.MenuHighlight;
+            BtnSalir.Font = new Font("Segoe UI", 13F);
+            BtnSalir.Location = new Point(542, 12);
+            BtnSalir.Name = "BtnSalir";
+            BtnSalir.Size = new Size(75, 40);
+            BtnSalir.TabIndex = 17;
+            BtnSalir.Text = "Salir";
+            BtnSalir.UseVisualStyleBackColor = false;
+            BtnSalir.Click += BtnSalir_Click;
+            // 
+            // BtnNuevo
+            // 
+            BtnNuevo.BackColor = SystemColors.MenuHighlight;
+            BtnNuevo.Font = new Font("Segoe UI", 13F);
+            BtnNuevo.Location = new Point(483, 175);
+            BtnNuevo.Name = "BtnNuevo";
+            BtnNuevo.Size = new Size(80, 40);
+            BtnNuevo.TabIndex = 18;
+            BtnNuevo.Text = "Limpiar";
+            BtnNuevo.UseVisualStyleBackColor = false;
+            BtnNuevo.Click += BtnNuevo_Click;
+            // 
+            // LblTitle
+            // 
+            LblTitle.AutoSize = true;
+            LblTitle.BackColor = SystemColors.Highlight;
+            LblTitle.Font = new Font("Showcard Gothic", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LblTitle.ForeColor = SystemColors.InactiveCaptionText;
+            LblTitle.Location = new Point(61, 17);
+            LblTitle.Name = "LblTitle";
+            LblTitle.Size = new Size(123, 26);
+            LblTitle.TabIndex = 19;
+            LblTitle.Text = "Horarios";
+            LblTitle.Click += label1_Click;
+            // 
+            // Horarios
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(624, 396);
+            Controls.Add(LblTitle);
+            Controls.Add(BtnNuevo);
+            Controls.Add(BtnSalir);
+            Controls.Add(BtnConsultar);
+            Controls.Add(BtnDelete);
+            Controls.Add(BtnUpdate);
+            Controls.Add(BtnIngresar);
+            Controls.Add(DtpHoraFin);
+            Controls.Add(DtpHoraInicio);
+            Controls.Add(LblFin);
+            Controls.Add(LblInicio);
+            Controls.Add(TxtDiaSemana);
+            Controls.Add(LblDia);
+            Controls.Add(TxtSalonID);
+            Controls.Add(LblSalon);
+            Controls.Add(TxtCursoID);
+            Controls.Add(LblCurso);
+            Controls.Add(TxtIDHorario);
+            Controls.Add(LblID);
+            Name = "Horarios";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Horarios";
+            Load += Horarios_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private Label LblID;
+        private TextBox TxtIDHorario;
+        private Label LblCurso;
+        private TextBox TxtCursoID;
+        private Label LblSalon;
+        private TextBox TxtSalonID;
+        private Label LblDia;
+        private TextBox TxtDiaSemana;
+        private Label LblInicio;
+        private Label LblFin;
+        private DateTimePicker DtpHoraInicio;
+        private DateTimePicker DtpHoraFin;
+        private Button BtnIngresar;
+        private Button BtnUpdate;
+        private Button BtnDelete;
+        private Button BtnConsultar;
+        private Button BtnSalir;
+        private Button BtnNuevo;
+        private Label LblTitle;
+    }
+}
---
+++ BaseDeDatos/BaseDeDatos/EAFIT-BaseDeDatos.UI/Horarios.cs
@@ -0,0 +1,176 @@
+﻿using EAFIT_BaseDeDatos.Core.EAFIT_FACADE;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace EAFIT_BaseDeDatos.UI
+{
+    public partial class Horarios : Form
+    {
+        Form InstanciaPpal;
+        bool Result = false;
+
+        public Horarios(Form ppal)
+        {
+            InitializeComponent();
+            InstanciaPpal = ppal;
+        }
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            InstanciaPpal.Close();
+        }
+
+
+        private void BtnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+        }
+
+        private void LimpiarFormulario()
+        {
+            TxtIDHorario.Text = "";
+            TxtCursoID.Text = "";
+            TxtSalonID.Text = "";
+            TxtDiaSemana.Text = "";
+            DtpHoraInicio.Text = "";
+            DtpHoraFin.Text = "";
+
+            BtnIngresar.Enabled = true;
+            BtnUpdate.Enabled = BtnDelete.Enabled = false;
+        }
+
+        private void BtnConsultar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtIDHorario.Text))
+            {
+                MessageBox.Show("Debe ingresar un ID de horario para consultar.", "Consulta de Horario", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int idHorario;
+            if (!int.TryParse(TxtIDHorario.Text, out idHorario))
+            {
+                MessageBox.Show("El ID del horario debe ser un número entero.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            BtnIngresar.Enabled = false;
+            BtnUpdate.Enabled = BtnDelete.Enabled = true;
+
+            FacadeHorarios InstSearch = new FacadeHorarios();
+            Dictionary<string, string> Result = InstSearch.ConsultarHorario(idHorario);
+
+            if (Result != null && Result.Count > 0)
+            {
+                TxtCursoID.Text = Result.GetValueOrDefault("curso_id_curso");
+                TxtSalonID.Text = Result.GetValueOrDefault("salon_id_salon");
+                TxtDiaSemana.Text = Result.GetValueOrDefault("dia_semana");
+                DtpHoraInicio.Text = Result.GetValueOrDefault("hora_inicio");
+                DtpHoraFin.Text = Result.GetValueOrDefault("hora_fin");
+            }
+            else
+            {
+                MessageBox.Show("No se encontró el horario especificado.", "Consulta de Horario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LimpiarFormulario();
+            }
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            ExecuteHorario(1);
+            if (Result)
+            {
+                MessageBox.Show("Horario insertado correctamente.", "Ingreso de Horario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LimpiarFormulario();
+            }
+            else
+                MessageBox.Show("Error al insertar el horario. Verifique los datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            ExecuteHorario(2);
+            if (Result)
+            {
+                MessageBox.Show("Horario actualizado correctamente.", "Actualización de Horario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LimpiarFormulario();
+            }
+            else
+                MessageBox.Show("Error al actualizar el horario. Verifique los datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtIDHorario.Text))
+            {
+                MessageBox.Show("Debe ingresar el ID del horario a eliminar.", "Eliminación de Horario", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int idHorario;
+            if (!int.TryParse(TxtIDHorario.Text, out idHorario))
+            {
+                MessageBox.Show("El ID del horario debe ser numérico.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            FacadeHorarios InstEliminar = new FacadeHorarios();
+            bool Result = InstEliminar.EliminarHorario(idHorario);
+
+            if (Result)
+            {
+                MessageBox.Show("Horario eliminado correctamente.", "Eliminación de Horario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LimpiarFormulario();
+            }
+            else
+                MessageBox.Show("Error al eliminar el horario.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        internal bool ExecuteHorario(int Accion)
+        {
+            Result = false;
+
+            if (string.IsNullOrEmpty(TxtCursoID.Text) ||
+                string.IsNullOrEmpty(TxtSalonID.Text) ||
+                string.IsNullOrEmpty(TxtDiaSemana.Text) ||
+                string.IsNullOrEmpty(DtpHoraInicio.Text) ||
+                string.IsNullOrEmpty(DtpHoraFin.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos para continuar.", "Datos incompletos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            Dictionary<string, string> ValuesInsert = new Dictionary<string, string>();
+            ValuesInsert.Add("id_horario", TxtIDHorario.Text);
+            ValuesInsert.Add("curso_id_curso", TxtCursoID.Text);
+            ValuesInsert.Add("salon_id_salon", TxtSalonID.Text);
+            ValuesInsert.Add("dia_semana", TxtDiaSemana.Text);
+            ValuesInsert.Add("hora_inicio", DtpHoraInicio.Text);
+            ValuesInsert.Add("hora_fin", DtpHoraFin.Text);
+
+            FacadeHorarios InstHorario = new FacadeHorarios();
+
+            if (Accion == 1)
+                Result = InstHorario.InsertarHorario(ValuesInsert);
+            else if (Accion == 2)
+                Result = InstHorario.ActualizarHorario(ValuesInsert);
+
+            return Result;
+        }
+
+        private void Horarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
