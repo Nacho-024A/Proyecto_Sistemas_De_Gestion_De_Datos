1a4ee48da291ff30488b11cb0b84ff6af6859aa8
Juan Fernando Hernández Arenas <141139460+RicketyDream@users.noreply.github.com>
2025-11-04 13:19:01 -0500

    Add files via upload

---
+++ BaseDeDatos/BaseDeDatos/EAFIT-BaseDeDatos.Core/EAFIT-FACADE/FacadeHorarios.cs
@@ -0,0 +1,32 @@
+﻿using EAFIT_BaseDeDatos.Core.EAFIT_MANAGER;
+using System.Collections.Generic;
+
+namespace EAFIT_BaseDeDatos.Core.EAFIT_FACADE
+{
+    public class FacadeHorarios
+    {
+        public bool InsertarHorario(Dictionary<string, string> ValuesHorario)
+        {
+            ManagerHorarios InsertHorario = new ManagerHorarios();
+            return InsertHorario.InsertarHorario(ValuesHorario);
+        }
+
+        public bool ActualizarHorario(Dictionary<string, string> ValuesHorario)
+        {
+            ManagerHorarios UpdateHorario = new ManagerHorarios();
+            return UpdateHorario.ActualizarHorario(ValuesHorario);
+        }
+
+        public bool EliminarHorario(int id_horario)
+        {
+            ManagerHorarios DeleteHorario = new ManagerHorarios();
+            return DeleteHorario.EliminarHorario(id_horario);
+        }
+
+        public Dictionary<string, string> ConsultarHorario(int id_horario)
+        {
+            ManagerHorarios GetHorario = new ManagerHorarios();
+            return GetHorario.ConsultarHorario(id_horario);
+        }
+    }
+}
