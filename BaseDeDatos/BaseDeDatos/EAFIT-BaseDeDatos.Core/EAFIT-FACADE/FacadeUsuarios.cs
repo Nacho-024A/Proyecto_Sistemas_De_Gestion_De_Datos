539a11d8e7a69af938bed2b872d6223a21a239c4
Andrés Osorio <144381281+Nacho-024A@users.noreply.github.com>
2025-11-18 16:24:21 -0500

    Refactor FacadeUsuarios to include new methods

--- BaseDeDatos/BaseDeDatos/EAFIT-BaseDeDatos.Core/EAFIT-FACADE/FacadeUsuarios.cs
+++ BaseDeDatos/BaseDeDatos/EAFIT-BaseDeDatos.Core/EAFIT-FACADE/FacadeUsuarios.cs
@@ -1,8 +1,10 @@
-﻿using EAFIT_BaseDeDatos.Core.EAFIT_BROKER;
+using EAFIT_BaseDeDatos.Core.EAFIT_BROKER;
+using EAFIT_BaseDeDatos.Core.EAFIT_ENTITIES;
 using EAFIT_BaseDeDatos.Core.EAFIT_MANAGER;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,10 @@ namespace EAFIT_BaseDeDatos.Core.EAFIT_FACADE
 {
     public class FacadeUsuarios
     {
-
-        public bool InsertarUsuario(Dictionary<string, string> ValuesUsuario)
+        public (bool, string) InsertarUsuario(Dictionary<string, string> ValuesUsuario)
         {
-            EAFIT_MANAGER.ManagerUsuarios InsertUser = new EAFIT_MANAGER.ManagerUsuarios();
-            return InsertUser.InsertarUsuario(ValuesUsuario);
-
+            ManagerUsuarios manager = new ManagerUsuarios();
+            return manager.InsertarUsuario(ValuesUsuario);
         }
         public bool ActualizarUsuario(Dictionary<string, string> ValuesUsuario)
         {
@@ -30,11 +30,36 @@ namespace EAFIT_BaseDeDatos.Core.EAFIT_FACADE
 
         }
 
+        public bool ActivarUsuario(string ValuesUsuario)
+        {
+            EAFIT_MANAGER.ManagerUsuarios InsertUser = new EAFIT_MANAGER.ManagerUsuarios();
+            return InsertUser.ActivarUsuario(ValuesUsuario);
+        }
+
         public Dictionary<string, string> ValidarUsuario(string UserSearch)
         {
 
             ManagerUsuarios ValidateUser = new ManagerUsuarios();
             return ValidateUser.ValidarUsuario(UserSearch);
         }
+
+        public List<KeyValuePair<int, string>> ObtenerFacultades()
+        {
+            ManagerUsuarios InstManager = new ManagerUsuarios();
+            return InstManager.ObtenerFacultades();
+        }
+
+        public List<KeyValuePair<int, string>> ObtenerCarreras(int idFacultad)
+        {
+            ManagerUsuarios InstManager = new ManagerUsuarios();
+            return InstManager.ObtenerCarrerasPorFacultad(idFacultad);
+        }
+
+        public List<KeyValuePair<int, string>> ObtenerPensums(int idFacultad)
+        {
+            ManagerUsuarios InstManager = new ManagerUsuarios();
+            return InstManager.ObtenerPensumsPorCarrera(idFacultad);
+        }
+
     }
 }
